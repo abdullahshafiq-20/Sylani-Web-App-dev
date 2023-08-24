18c8c91436b5262c0d8f14209628b1268f348ea9
Abdullah Shafiq <as053266@gmail.com>
2023-08-25 00:26:58 +0500

    Wednesday Assign Added

---
+++ Javascript/Arrays/FromGithub.js
@@ -0,0 +1,11 @@
+let fruits = ['grapes', 'apple', 'watermelon', 'mango', 'peach', 'nashpati'];
+// fruits.push('peach');
+// fruits.pop();
+// fruits.unshift('nashpati');
+// fruits.shift();
+// let eaten = fruits.slice(1 , 3);
+
+// console.log(eaten);
+fruits.splice(1, 3 , 'kiwi' , 'banana');
+
+console.log(fruits);
